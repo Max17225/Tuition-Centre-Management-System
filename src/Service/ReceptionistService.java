dec1e027bae8eae85063f4b9abfbe333d12353fc
kunlalala <167629295+kunlalala@users.noreply.github.com>
2025-07-19 12:02:02 +0800

    Update ReceptionistService.java

--- src/Service/ReceptionistService.java
+++ src/Service/ReceptionistService.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Service;
 
-/**
- *
- * @author nengz
- */
+import Util.DataManager;
+import DataModel.Enrollment;
+
 public class ReceptionistService {
-    
+
+    public static void registerEnrollmentOnly(Enrollment enrollment) {
+        DataManager<Enrollment> manager = DataManager.of(Enrollment.class);
+        manager.appendOne(enrollment);
+    }
 }
